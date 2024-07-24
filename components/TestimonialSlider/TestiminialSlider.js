e0eeeef3ce3177726cc3596e8935fe316c75a09c
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-07-24 09:49:05 +0100

    modify Slider

--- components/TestimonialSlider/TestiminialSlider.js
+++ components/TestimonialSlider/TestiminialSlider.js
@@ -34,8 +34,8 @@ const TestiminialSlider = () => {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
