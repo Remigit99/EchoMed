f205cd551e8d8f4f145ee8cd44464cd9bfbc5d4e
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-07-24 13:44:06 +0100

    modify navbar

--- components/TestimonialSlider/TestiminialSlider.js
+++ components/TestimonialSlider/TestiminialSlider.js
@@ -1,19 +1,9 @@
 "use client";
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-// import { Carousel } from "react-responsive-carousel";
+
 import { testimonials } from "@/Data/testimonialData";
 import Image from "next/image";
 import styles from "./testimonial.module.css";
 
-// Swiper components, modules and styles
-// import { Autoplay, Navigation, Pagination } from "swiper/modules";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-
-// Our custom button component
-// import SliderButtons from "./SliderButtons";
 
 // React Slick
 import Slider from "react-slick";
@@ -24,19 +14,20 @@ import { NextArrow,PrevArrow } from "../TesimonialArrows";
 const TestiminialSlider = () => {
   let settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    adaptiveHeight: true,
     responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 1,
-            infinite: true,
+            infinite: false,
             dots: true
           }
         },
@@ -52,7 +43,9 @@ const TestiminialSlider = () => {
           breakpoint: 480,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            initialSlide: 1
+
           }
         }
       ]
