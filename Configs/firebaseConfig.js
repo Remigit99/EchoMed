af70e82f722f940eb647b5deadd6f43d5f26e514
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2024-08-05 19:48:34 +0100

    modify firebaseConfig

--- Configs/firebaseConfig.js
+++ Configs/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_APIKEY),
+  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN),
+  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECTID),
+  storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID),
+  appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID)
 };
 
 // Initialize Firebase
